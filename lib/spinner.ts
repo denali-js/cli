daf72bbf9e9a017f944df683a598c848c1a095da
Dave Wasmer <davewasmer@gmail.com>
2017-09-08 22:43:00 -0600

    feat: ensure spinner does not interfere with other process output

--- lib/spinner.ts
+++ lib/spinner.ts
@@ -1,9 +1,12 @@
 import { fork } from 'child_process';
 import * as path from 'path';
 import { Socket } from 'net';
+import { Writable } from 'stream';
+import * as readline from 'readline';
 
 let childSpinner = fork(path.join(__dirname, 'spinner-child.js'));
 childSpinner.unref();
+
 let childChannel = <Socket>(<any>childSpinner).channel;
 childChannel.unref();
 
@@ -39,6 +42,7 @@ export default {
 
 let operationsQueue: { operation: string, args: any[] }[] = [];
 let inFlight = false;
+let spinnerIsActive = false;
 
 function queue(operation: string, ...args: any[]): void {
   operationsQueue.push({ operation, args });
@@ -50,14 +54,39 @@ function flushQueue() {
     return;
   }
   if (operationsQueue.length > 0) {
-    inFlight = true;
     let nextOperation = operationsQueue.shift();
+    inFlight = true;
+    if (nextOperation.operation === 'start') {
+      spinnerIsActive = true;
+    }
     childChannel.ref();
     childSpinner.send(nextOperation);
     childSpinner.once('message', () => {
       childChannel.unref();
       inFlight = false;
+      if (nextOperation.operation !== 'start') {
+        spinnerIsActive = false;
+      }
       flushQueue();
     });
   }
 }
+
+export function wrapOutputStream(stream: Writable) {
+  let originalWrite = stream.write.bind(stream);
+  let mockOriginalStream = <any>{ write: originalWrite };
+  stream.write = function wrappedWrite(chunk: any, encoding?: any, callback?: any) {
+    if (spinnerIsActive) {
+      readline.clearLine(mockOriginalStream, 0);
+      readline.cursorTo(mockOriginalStream, 0);
+      chunk = chunk.toString();
+      if (!chunk.endsWith('\n')) {
+        chunk = chunk + '\n';
+      }
+    }
+    return originalWrite(chunk, encoding, callback);
+  };
+}
+
+wrapOutputStream(<any>process.stdout);
+wrapOutputStream(<any>process.stderr);
