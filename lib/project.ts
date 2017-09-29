07e6c424a5794c95bb14287a96ea294aa3aa6b24
Chris Watts <seawatts@users.noreply.github.com>
2017-09-29 12:29:10 -0700

    fix: fix dedent import (#34)

--- lib/project.ts
+++ lib/project.ts
@@ -5,7 +5,7 @@ import {
 } from 'lodash';
 import * as path from 'path';
 import * as fs from 'fs';
-import dedent from 'dedent-js';
+import * as dedent from 'dedent-js';
 import * as nsp from 'nsp';
 import * as broccoli from 'broccoli';
 import * as rimraf from 'rimraf';
