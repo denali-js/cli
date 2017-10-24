1c4fe53ac1340bedf8f636c7ad1dc1b18cc1d646
Dave Wasmer <davewasmer@gmail.com>
2017-10-23 23:59:49 -0600

    feat: add manifest lib/index.js to addon blueprint

---
+++ blueprints/addon/files/__name__/lib/index.js
@@ -0,0 +1,8 @@
+// Use this file to re-export anything you want users to be able to directly import, i.e.
+//
+//   export { default as AuthBeforeFilter } from './auth-before-filter';
+//
+// Then users would be able to:
+//
+//   import { AuthBeforeFilter } from '<%= name %>';
+//
