e8977e8227a018a57e13df2fc7b7fcc057abe6d8
Dave Wasmer <davewasmer@gmail.com>
2018-01-12 22:38:42 -0700

    refactor bin/denali.ts to simplify, make more robust

--- bin/denali.ts
+++ bin/denali.ts
@@ -6,7 +6,8 @@ import * as path from 'path';
 import * as fs from 'fs';
 import * as NestedError from 'nested-error-stacks';
 import * as resolve from 'resolve';
-import findup = require('findup-sync');
+import { sync as readPkgUp } from 'read-pkg-up';
+import * as pkgDir from 'pkg-dir';
 
 /* tslint:disable:no-console */
 
@@ -18,36 +19,33 @@ if (!satisfies(process.version, '>=7.6')) {
   throw new Error(`Denali requires node version >= 7.6, you used ${ version }`);
 }
 
-let localPkg: any = null;
+let projectPkg: any = null;
 let cliPkg: any;
 let cliBootstrapPath: string;
 let source: string;
 
 try {
-  let pkgPath = findup('package.json');
-  if (pkgPath) {
-    try {
-      localPkg = require(pkgPath);
-      let pkgDir = path.dirname(path.resolve(pkgPath));
-      let localCliMain = resolve.sync('denali-cli', { basedir: pkgDir });
-      let localCliDir = path.dirname(findup('package.json', { cwd: localCliMain }));
-      cliPkg = require(path.join(localCliDir, 'package.json'));
-      process.chdir(pkgDir);
-      source = fs.lstatSync(localCliDir).isSymbolicLink() ? 'linked' : 'local';
-      cliBootstrapPath = path.join(localCliDir, 'dist/lib/bootstrap');
-    } catch (e) {
-      cliPkg = path.join(__dirname, '../package.json');
-      cliBootstrapPath = path.join(__dirname, '../lib/bootstrap');
-      source = 'global';
-    }
+  try {
+    projectPkg = readPkgUp().pkg;
+    let projectDir = pkgDir();
+    let localCliMain = resolve.sync('denali-cli', { basedir: projectDir });
+    let localCliDir = pkgDir(localCliMain);
+    cliPkg = readPkgUp({ cwd: localCliMain });
+    process.chdir(projectDir);
+    source = fs.lstatSync(localCliDir).isSymbolicLink() ? 'linked' : 'local';
+    cliBootstrapPath = path.join(localCliDir, 'dist/lib/bootstrap');
+  } catch (e) {
+    cliPkg = readPkgUp({ cwd: __dirname }).pkg;
+    cliBootstrapPath = path.join(__dirname, '../lib/bootstrap');
+    source = 'global';
+  }
 
-    process.stdout.write(`cli v${ cliPkg.version } [${ source }]`);
+  process.stdout.write(`cli v${ cliPkg.version } [${ source }]`);
 
-    try {
-      require(cliBootstrapPath).default(localPkg);
-    } catch (error) {
-      throw new NestedError('\nError encountered while starting up denali-cli', error);
-    }
+  try {
+    require(cliBootstrapPath).default(projectPkg);
+  } catch (error) {
+    throw new NestedError('\nError encountered while starting up denali-cli', error);
   }
 } catch (e) {
   console.error(chalk.red(e.stack || e.message || e));
